3ec8265d16986b936cfe16098441d99ab26d3568
Kim Carter <Kim.Carter@binarymist.net>
2018-06-30 14:38:04 +1200

    Added config attributes & relationships to validation schema

--- src/api/orchestration/schemas/buildUserConfig.js
+++ src/api/orchestration/schemas/buildUserConfig.js
@@ -2,7 +2,9 @@ const Joi = require('joi');
 
 const buildUserConfigSchema = Joi.object({
   data: Joi.object({
-    type: Joi.string().required().valid('testRun')
+    type: Joi.string().required().valid('testRun'),
+    attributes: Joi.object(),
+    relationships: Joi.object()
   }),
   included: Joi.array()
 });
