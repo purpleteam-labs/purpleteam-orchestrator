a7e63e1a5c3e2ddcb587280459b9c164df5174b1
Kim Carter <Kim.Carter@binarymist.net>
2018-07-05 12:58:42 +1200

    Added validation to the /test route

--- src/api/orchestration/routes/post.js
+++ src/api/orchestration/routes/post.js
@@ -20,6 +20,7 @@ const internals = {
   }
 };
 
+
 module.exports = [{
   method: 'POST',
   path: '/testplan',
@@ -35,9 +36,11 @@ module.exports = [{
 }, {
   method: 'POST',
   path: '/test',
-  handler: async (request, respToolkit) => { // eslint-disable-line no-unused-vars
+  options: {
+    validate: internals.validate,
+    handler: async (request, respToolkit) => { // eslint-disable-line no-unused-vars
 
-    //throw Boom.badImplementation('Oh, dear, not sure what happened.', 'Some data to be used serverside from data property');
+      //throw Boom.badImplementation('Oh, dear, not sure what happened.', 'Some data to be used serverside from data property');
 
     
 
@@ -50,34 +53,32 @@ module.exports = [{
 
 
 
-    const { model } = request.server.app;
-    const runJob = await model.testTeamAttack(request.payload);
+      const { model } = request.server.app;
+      const runJob = await model.testTeamAttack(request.payload);
 
 
 
 
 
-// Start each tester with their jobs (provide argument for planOnly)
-//    If planOnly is passed, when the plan is returned, the tester is done
-//    Always return the plan to the CLI
+  // Start each tester with their jobs (provide argument for planOnly)
+  //    If planOnly is passed, when the plan is returned, the tester is done
+  //    Always return the plan to the CLI
 
-    return respToolkit.response(runJob).code(202);
-    // If we return JSON API data then it needs to meet the jsonapi spec (http://jsonapi.org/format/#content-negotiation-servers) and the media type (Content-Type) must be application/vnd.api+json
-
-
-//    Provide feedback to the CLI on progress if requested
-
-// Once all testers have returned their results (by way of SSE):
-//    Coalate results / report(s) once all testers are done, generate report in build user specified formats and return them
+      return respToolkit.response(runJob).code(202);
+      // If we return JSON API data then it needs to meet the jsonapi spec (http://jsonapi.org/format/#content-negotiation-servers) and the media type (Content-Type) must be application/vnd.api+json
 
 
+  //    Provide feedback to the CLI on progress if requested
+
+  // Once all testers have returned their results (by way of SSE):
+  //    Coalate results / report(s) once all testers are done, generate report in build user specified formats and return them
 
 
 
 
 
+
+
+    }
   }
-
 }];
-
-
