58917e4356b5a8cc7057bcd68dc0bff3751e29bb
Kim Carter <Kim.Carter@binarymist.net>
2018-08-11 11:32:24 +1200

    Linting redis testerWatcher

--- src/api/orchestration/subscribers/testerWatcher.js
+++ src/api/orchestration/subscribers/testerWatcher.js
@@ -4,23 +4,23 @@ let log;
 let redisOptions;
 const subscribers = {};
 
-const init = (options) => {
-  log = options.log;
-  redisOptions = options.redis;
-
-  return {subscribe};
-};
-
-
 const subscribe = (redisChannel, callback) => {
   subscribers[redisChannel] = redis.createClient(redisOptions);
   subscribers[redisChannel].subscribe(redisChannel);
-  
+
   subscribers[redisChannel].on('error', (error) => {
     log.error(`Redis error: ${error}`);
   });
 
   subscribers[redisChannel].on('message', callback);
+};
+
+
+const init = (options) => {
+  log = options.log; // eslint-disable-line prefer-destructuring
+  redisOptions = options.redis;
+
+  return { subscribe };
 };
 
 
