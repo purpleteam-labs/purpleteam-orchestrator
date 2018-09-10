3fa714cab7b0e6d4a7315fd5fd6a574b25d4e2bd
Kim Carter <Kim.Carter@binarymist.net>
2018-09-11 10:35:41 +1200

    Formatted, linted schema

--- src/api/orchestration/schemas/buildUserConfig.js
+++ src/api/orchestration/schemas/buildUserConfig.js
@@ -3,249 +3,174 @@ const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
 
 
 const schema = {
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "$ref": "#/definitions/BuildUserConfig",
-  "definitions": {
-      "BuildUserConfig": {
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-              "data": {
-                  "$ref": "#/definitions/Data"
-              },
-              "included": {
-                  "type": "array",
-                  "items": {
-                      "$ref": "#/definitions/Included"
-                  }
-              }
-          },
-          "required": [
-              "data",
-              "included"
-          ],
-          "title": "BuildUserConfig"
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  $ref: '#/definitions/BuildUserConfig',
+  definitions: {
+    BuildUserConfig: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        data: { $ref: '#/definitions/Data' },
+        included: {
+          type: 'array',
+          items: { $ref: '#/definitions/TopLevelResourceObject' }
+        }
       },
-      "Data": {
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-              "type": {
-                  "type": "string"
-              },
-              "attributes": {
-                  "$ref": "#/definitions/DataAttributes"
-              },
-              "relationships": {
-                  "$ref": "#/definitions/Relationships"
-              }
-          },
-          "required": [
-              "attributes",
-              "relationships",
-              "type"
-          ],
-          "title": "Data"
+      required: [
+        'data',
+        'included'
+      ],
+      title: 'BuildUserConfig'
+    },
+    Data: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        type: { type: 'string' },
+        attributes: { $ref: '#/definitions/DataAttributes' },
+        relationships: { $ref: '#/definitions/Relationships' }
       },
-      "DataAttributes": {
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-              "version": {
-                  "type": "string"
-              },
-              "sutAuthentication": {
-                  "$ref": "#/definitions/SutAuthentication"
-              },
-              "sutIp": {
-                  "type": "string"
-              },
-              "sutPort": {
-                  "type": "integer"
-                  
-              },
-              "sutProtocol": {
-                  "type": "string"
-              },
-              "browser": {
-                  "type": "string"
-              },
-              "loggedInIndicator": {
-                  "type": "string"
-              },
-              "reportFormats": {
-                  "type": "array",
-                  "items": {
-                      "type": "string"
-                  }
-              }
-          },
-          "required": [
-              "browser",
-              "loggedInIndicator",
-              "reportFormats",
-              "sutAuthentication",
-              "sutIp",
-              "sutPort",
-              "sutProtocol",
-              "version"
-          ],
-          "title": "DataAttributes"
+      required: [
+        'attributes',
+        'relationships',
+        'type'
+      ],
+      title: 'Data'
+    },
+    DataAttributes: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        version: { type: 'string' },
+        sutAuthentication: { $ref: '#/definitions/SutAuthentication' },
+        sutIp: { type: 'string' },
+        sutPort: { type: 'integer' },
+        sutProtocol: { type: 'string' },
+        browser: { type: 'string' },
+        loggedInIndicator: { type: 'string' },
+        reportFormats: {
+          type: 'array',
+          items: { type: 'string' }
+        }
       },
-      "SutAuthentication": {
-          "type": "object",
-          "additionalProperties": true,
-          "properties": {
-              "route": {
-                  "type": "string"
-              },
-              "usernameFieldLocater": {
-                  "type": "string"
-              },
-              "passwordFieldLocater": {
-                  "type": "string"
-              },
-              "submit": {
-                  "type": "string"
-              },
-              "expectedResponseSuccess": {
-                "type": "string"
-              },
-              "expectedResponseFail": {
-                "type": "string"
-              }  
-          },
-          oneOf: [
-            {
-              required: [
-                "expectedResponseSuccess"
-              ],
-            }, {
-              required: [
-                "expectedResponseFail"
-              ]
-            }
-          ],
-          "required": [
-              "passwordFieldLocater",
-              "route",
-              "submit",
-              "usernameFieldLocater"
-          ],
-          "title": "SutAuthentication"
+      required: [
+        'browser',
+        'loggedInIndicator',
+        'reportFormats',
+        'sutAuthentication',
+        'sutIp',
+        'sutPort',
+        'sutProtocol',
+        'version'
+      ],
+      title: 'DataAttributes'
+    },
+    SutAuthentication: {
+      type: 'object',
+      additionalProperties: true,
+      properties: {
+        route: { type: 'string' },
+        usernameFieldLocater: { type: 'string' },
+        passwordFieldLocater: { type: 'string' },
+        submit: { type: 'string' },
+        expectedResponseSuccess: { type: 'string' },
+        expectedResponseFail: { type: 'string' }
       },
-      "Relationships": {
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-              "data": {
-                  "type": "array",
-                  "items": {
-                      "$ref": "#/definitions/Datum"
-                  }
-              }
-          },
-          "required": [
-              "data"
-          ],
-          "title": "Relationships"
+      oneOf: [
+        {
+          required: [
+            'expectedResponseSuccess'
+          ]
+        }, {
+          required: [
+            'expectedResponseFail'
+          ]
+        }
+      ],
+      required: [
+        'passwordFieldLocater',
+        'route',
+        'submit',
+        'usernameFieldLocater'
+      ],
+      title: 'SutAuthentication'
+    },
+    Relationships: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        data: {
+          type: 'array',
+          items: { $ref: '#/definitions/Datum' }
+        }
       },
-      "Datum": {
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-              "type": {
-                  "type": "string"
-              },
-              "id": {
-                  "type": "string"
-              }
-          },
-          "required": [
-              "id",
-              "type"
-          ],
-          "title": "Datum"
+      required: [
+        'data'
+      ],
+      title: 'Relationships'
+    },
+    Datum: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        type: { type: 'string' },
+        id: { type: 'string' }
       },
-      "Included": {
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-              "type": {
-                  "type": "string"
-              },
-              "id": {
-                  "type": "string"
-              },
-              "attributes": {
-                  "$ref": "#/definitions/IncludedAttributes"
-              },
-              "relationships": {
-                  "$ref": "#/definitions/Relationships"
-              }
-          },
-          "required": [
-              "attributes",
-              "id",
-              "type"
-          ],
-          "title": "Included"
+      required: [
+        'id',
+        'type'
+      ],
+      title: 'Datum'
+    },
+    TopLevelResourceObject: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        type: { type: 'string' },
+        id: { type: 'string' },
+        attributes: { $ref: '#/definitions/AttributesObjOfTopLevelResourceObject' },
+        relationships: { $ref: '#/definitions/Relationships' }
       },
-      "IncludedAttributes": {
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-              "username": {
-                  "type": "string"
-              },
-              "password": {
-                  "type": "string"
-              },
-              "aScannerAttackStrength": {
-                  "type": "string"
-              },
-              "aScannerAlertThreshold": {
-                  "type": "string"
-              },
-              "alertThreshold": {
-                  "type": "integer"
-              },
-              "attackFields": {
-                  "type": "array",
-                  "items": {
-                      "$ref": "#/definitions/AttackField"
-                  }
-              },
-              "method": {
-                  "type": "string"
-              },
-              "submit": {
-                  "type": "string"
-              }
-          },
-          "required": [],
-          "title": "IncludedAttributes"
+      required: [
+        'attributes',
+        'id',
+        'type'
+      ],
+      title: 'Included'
+    },
+    AttributesObjOfTopLevelResourceObject: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        username: { type: 'string' },
+        password: { type: 'string' },
+        aScannerAttackStrength: { type: 'string' },
+        aScannerAlertThreshold: { type: 'string' },
+        alertThreshold: { type: 'integer' },
+        attackFields: {
+          type: 'array',
+          items: { $ref: '#/definitions/AttackField' }
+        },
+        method: { type: 'string' },
+        submit: { type: 'string' }
       },
-      "AttackField": {
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-              "name": {
-                  "type": "string"
-              },
-              "value": {
-                  "type": "string"
-              },
-              "visible": {
-                  "type": "boolean"
-              }
-          },
-          "required": [
-              "name",
-              "value"
-          ],
-          "title": "AttackField"
-      }
+      required: [],
+      title: 'AttributesObjOfTopLevelResourceObject'
+    },
+    AttackField: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        name: { type: 'string' },
+        value: { type: 'string' },
+        visible: { type: 'boolean' }
+      },
+      required: [
+        'name',
+        'value'
+      ],
+      title: 'AttackField'
+    }
   }
 };
 
